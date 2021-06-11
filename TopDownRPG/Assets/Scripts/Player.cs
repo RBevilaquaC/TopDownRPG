888b43c26a842b692058e955d4fa0e504aa32a27
Roberto Bevilaqua <rbdc.eng@uea.edu.br>
2021-06-11 09:04:40 -0400

    Curso finalizado
    curso rpg topdown finalizado

--- TopDownRPG/Assets/Scripts/Player.cs
+++ TopDownRPG/Assets/Scripts/Player.cs
@@ -1,31 +1,68 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Mover
 {
-    private BoxCollider2D boxCollider;
-
-    private Vector3 moveDelta;
-
-    private void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
+    public SpriteRenderer spriteRenderer;
+    private bool isAlive = true;
 
     private void FixedUpdate()
     {
-        moveDelta = Vector3.zero;
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        moveDelta = new Vector3(x, y, 0);
-        if (moveDelta.x > 0) 
-            transform.localScale = Vector3.one;
-        else if (moveDelta.x < 0) 
-            transform.localScale = new Vector3(-1,1,1);
+        if(isAlive)
+            UpdateMotor(new Vector3(x, y, 0));
+    }
 
-        transform.Translate(moveDelta * Time.deltaTime);
+    // Overrides
+    protected override void ReceiveDamage(Damage dmg)
+    {
+        if (!isAlive)
+            return;
 
+        base.ReceiveDamage(dmg);
+        GameManager.instance.OnHitpointChange();
+    }
+    protected override void Death()
+    {
+        isAlive = false;
+        GameManager.instance.deathMenuAnim.SetTrigger("Show");
+    }
+
+
+    // Public methods
+    public void SwapSprite(int skinId)
+    {
+        spriteRenderer.sprite = GameManager.instance.playerSprites[skinId];
+    }
+    public void OnLevelUp()
+    {
+        maxHitpoint++;
+        hitpoint = maxHitpoint;
+    }
+    public void SetLevel(int level)
+    {
+        for (int i = 0; i < level; i++)
+            OnLevelUp();
+    }
+    public void Heal(int healingAmount)
+    {
+        if (hitpoint == maxHitpoint)
+            return;
+
+        hitpoint += healingAmount;
+        if (hitpoint > maxHitpoint)
+            hitpoint = maxHitpoint;
+        GameManager.instance.ShowText("+" + healingAmount.ToString() + "hp", 25, Color.green, transform.position, Vector3.up * 30, 1.0f);
+        GameManager.instance.OnHitpointChange();
+    }
+    public void Respawn()
+    {
+        Heal(maxHitpoint);
+        isAlive = true;
+        lastImmune = Time.time;
+        pushDirection = Vector3.zero;
     }
 }
